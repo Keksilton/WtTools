b41a40de613abcfab0ed99ebe4f988dc03b11c4a
Keksilton <13134670+Keksilton@users.noreply.github.com>
2021-10-30 20:46:16 -0400

    Fix accidental using removal.

--- WtTools.Formats/WrlpInfo.cs
+++ WtTools.Formats/WrlpInfo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WtTools.Formats.Extensions;
+using ZstdNet;
 
 namespace WtTools.Formats
 {
